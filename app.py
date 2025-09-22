ee44f70a718b38b6ceb6440f4aa6f15d77144851
Lgerman-00 <lgerman.00g@gmail.com>
2025-09-21 19:46:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from Arbol import procesar_respuestas
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-app = Flask(__name__)
+
+os.chdir(BASE_DIR)
+
+app = Flask(
+    __name__,
+    template_folder=BASE_DIR,
+    static_folder=BASE_DIR,
+    static_url_path='' 
+)
 
 @app.route('/')
 def index():
+
     return render_template('PlantaHTML.html')
 
 @app.route('/procesar_respuestas', methods=['POST'])
@@ -12,24 +23,29 @@ def procesar():
     datos = request.get_json()
     print("DATOS RECIBIDOS:", datos)
     try:
-        # Forzar tipos numéricos seguros
-        for campo in ["amin", "altura", "temp", "ph"]:
+
+      for campo in ["amin", "altura", "temp", "ph"]:
             if campo in datos:
                 try:
                     datos[campo] = float(datos[campo])
-                except:
+                except Exception:
                     pass
-        for campo in ["estado", "forma", "herbacea", "fruta", "hoja", "uso", "cflor", "heladas", "riego", "sol", "velocidad", "phCond", "suelo", "CMO"]:
+
+        for campo in ["estado", "forma", "herbacea", "fruta", "hoja", "uso", "cflor",
+                      "heladas", "riego", "sol", "velocidad", "phCond", "suelo", "CMO"]:
             if campo in datos:
-                try:
-                    datos[campo] = int(datos[campo])
-                except:
-                    pass
+              try:
+                 datos[campo] = int(datos[campo])
+                except Exception:
+                   pass
+
+  
 
         imagen_url = procesar_respuestas(datos)
-
         return jsonify({'imagen_url': imagen_url})
+
     except Exception as e:
+        print("ERROR:", repr(e))
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
